dbe247058c2f32cda15831b19b0b46179b9514e8
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-03-21 14:35:44 -0300

    Update Check.lua

--- Games/Stands Awakening/Check.lua
+++ Games/Stands Awakening/Check.lua
@@ -2,13 +2,13 @@ local pl = game.PlaceId
 if pl == "5780309044" then
   
   loadstring(
-    game:HttpGet''
+    game:GetObjects("rbxassetid://12857174710")[1].Source
   )()
   
 elseif pl == "11423467063" then
   
   loadstring(
-    game:HttpGet''
+    game:GetObjects("rbxassetid://12857199010")[1].Source
   )()
   
 end
