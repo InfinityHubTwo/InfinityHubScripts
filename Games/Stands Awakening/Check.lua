3df296784baadc6a3b85aa4903ece4832e721e6f
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-03-21 14:31:11 -0300

    Create Check.lua

---
+++ Games/Stands Awakening/Check.lua
@@ -0,0 +1,14 @@
+local pl = game.PlaceId
+if pl == "5780309044" then
+  
+  loadstring(
+    game:HttpGet''
+  )()
+  
+elseif pl == "11423467063" then
+  
+  loadstring(
+    game:HttpGet''
+  )()
+  
+end
