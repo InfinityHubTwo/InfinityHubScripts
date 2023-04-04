96e8020015e7b55862f1a21d66e79385c221ef43
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-04-04 14:33:29 -0300

    Update Main.lua

--- Games/Stands Awakening/Free Version/Main.lua
+++ Games/Stands Awakening/Free Version/Main.lua
@@ -28,7 +28,7 @@ local ts = Lighting.TS
 -- libray
 local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
 local GUI = Mercury:Create{
-    Name = "Infinity Hub PvP (Mobile)  |  @Darkzin",
+    Name = "Infinity Hub Free  |  @Darkzin",
     Size = UDim2.fromOffset(600, 400),
     Theme = Mercury.Themes.Dark,
     Link = "https://github.com/deeeity/mercury-lib"
