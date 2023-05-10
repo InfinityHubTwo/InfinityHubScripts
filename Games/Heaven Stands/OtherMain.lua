848598b05349f80e080b1bde6610302a8c15cb6e
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-05-10 15:23:16 -0300

    Create OtherMain.lua

---
+++ Games/Heaven Stands/OtherMain.lua
@@ -0,0 +1,16 @@
+getgenv().Settings = {
+    Farming = {
+        ["Box Farm"] = false,
+        ["Cosmic Gate"] = false,
+    },
+    InstaKillBosses = {
+        ["Vergil"] = false,
+        ["DIO"] = false,
+        ["Garou"] = false,
+    },
+    CharacterFunctions = {
+        ["Disable Character Functions"] = false,
+    }
+}
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/InfinityHubTwo/InfinityHubScripts/main/Games/Heaven%20Stands/MainCode.lua'))()
