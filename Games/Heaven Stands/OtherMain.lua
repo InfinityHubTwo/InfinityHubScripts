37035f5db939b77bfc728b70c8b9ebabf5ac1ada
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-06-04 14:45:03 -0300

    Update OtherMain.lua

--- Games/Heaven Stands/OtherMain.lua
+++ Games/Heaven Stands/OtherMain.lua
@@ -1,16 +1,28 @@
-getgenv().Settings = {
-    Farming = {
+getgenv().Heaven_Stands = {
+    ["Farming Options"] = {
         ["Box Farm"] = false,
-        ["Cosmic Gate"] = false,
     },
-    InstaKillBosses = {
-        ["Vergil"] = false,
+    ["Insta Kill Bosses"] = {
         ["DIO"] = false,
         ["Garou"] = false,
+        ["Minos Prime"] = false,
     },
-    CharacterFunctions = {
-        ["Disable Character Functions"] = false,
-    }
-}
+    ["Player Options"] = {
+        ["Character"] = {
+            ["Disable Character Functions"] = false,
+            ["Disable Animate"] = false,
+        },
+        ["Others Options"] = {
+            ["Enable Speed and Jump"] = false,
+            ["WalkSpeed"] = "", -- normal is 16
+            ["JumpPower"] = "", -- normal is 50
+        },
+    },
+    ["Visual Options"] = {
+        ["Destroy Spirits"] = false,
+        ["Vergil 2 Swords"] = false,
+    },
+};
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/InfinityHubTwo/InfinityHubScripts/main/Games/Heaven%20Stands/MainCode.lua'))()
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/InfinityHubTwo/InfinityHubScripts/main/Games/Heaven%20Stands/MainCode.lua'))("Heaven_Stands");
+
