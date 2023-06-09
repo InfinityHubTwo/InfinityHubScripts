df3d8fb55c0511d193d7074baa889e138593538d
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-06-09 14:05:41 -0300

    Create Code.lua

---
+++ Games/Stands Awakening/FakeStand/Code.lua
@@ -0,0 +1,22 @@
+--[[
+Script made by darkzin
+How to use:
+    in "Stand Name" set a stand name
+    if you are in doubt, there is a link below with the names of the stands
+
+    in "Select Slot"
+    place the slot you want the FakeStand to appear in
+]]
+getgenv().Fake_Stand = {
+    ["Settings"] = {
+        ["Stand Name"] = "",
+        ["Select Slot"] = "",
+    },
+    ["Stands Names"] = {
+        "https://pastebin.com/raw/U72TkERs",
+    }
+};
+
+loadstring(game:HttpGetAsync(
+    'https://raw.githubusercontent.com/InfinityHubTwo/InfinityHubScripts/main/Games/Stands%20Awakening/FakeStand/Main.lua'
+))("Fake_Stands");
