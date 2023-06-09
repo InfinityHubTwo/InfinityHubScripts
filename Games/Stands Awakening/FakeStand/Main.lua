afd211792463ca1ca26175d8bda7b378cf59156c
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-06-09 16:28:04 -0300

    Update Main.lua

--- Games/Stands Awakening/FakeStand/Main.lua
+++ Games/Stands Awakening/FakeStand/Main.lua
@@ -2,6 +2,17 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Viewports = ReplicatedStorage.Viewports
 local Stands = Viewports.Stands
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+
+
+
+-- notifications
+local hi = Instance.new("Sound") hi.Name = "Notification_Sound" hi.SoundId = "http://www.roblox.com/asset/?id=6026984224" hi.Volume = 5 hi.archivable = false hi.Parent = game.Workspace
+hi:Play() wait(.46)
+Notification:Notify( {Title = "Fake Stand", Description = "generating stand..."}, {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 3, Type = "image"}, {Image = "http://www.roblox.com/asset/?id=7733964640", ImageColor = Color3.fromRGB(179, 255, 2), Callback = function(State) print(tostring(State)) end} ) wait(.25) workspace.Notification_Sound:Destroy()
+wait(4)
+
 
 
 -- destroy stand in slot
@@ -11,6 +22,7 @@ for _, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.StandStorage.
     end
 end
 wait(1)
+
 
 
 -- dupe stand
@@ -25,3 +37,28 @@ for _, v in pairs(Stands:GetChildren()) do
         game:GetService("Players").LocalPlayer.PlayerGui.StandStorage.Right.ImageLabel.ItemSlots.Slots[getgenv().Fake_Stand["Settings"]["Select Slot"]].Button.ExoticTier.Visible = true
     end
 end
+wait(1)
+
+
+
+-- clone animation
+for _, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.StandStorage.ports:GetChildren()) do
+    if v:IsA("Model") and v.Name == "npc" then
+        local npcClone = v:Clone() wait(.25)
+        npcClone.Parent = game:GetService("Players").LocalPlayer.PlayerGui.StandStorage.Right.ImageLabel.ItemSlots.Slots[
+            getgenv().Fake_Stand["Settings"]["Select Slot"]
+        ].ViewportFrame.WorldModel[getgenv().Fake_Stand["Settings"]["Stand Name"]]
+    end
+end
+
+
+
+-- credits
+local hi = Instance.new("Sound") hi.Name = "Notification_Sound" hi.SoundId = "http://www.roblox.com/asset/?id=6026984224" hi.Volume = 5 hi.archivable = false hi.Parent = game.Workspace
+hi:Play() wait(.46)
+Notification:Notify( {Title = "successfully", Description = "The stand you selected was successfully generated. | stand you selected: ".. tostring(getgenv().Fake_Stand["Settings"]["Stand Name"])}, {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 8, Type = "image"}, {Image = "http://www.roblox.com/asset/?id=7733964640", ImageColor = Color3.fromRGB(2, 255, 23), Callback = function(State) print(tostring(State)) end} ) wait(.25) workspace.Notification_Sound:Destroy()
+wait(1)
+local hi = Instance.new("Sound") hi.Name = "Notification_Sound" hi.SoundId = "http://www.roblox.com/asset/?id=6026984224" hi.Volume = 5 hi.archivable = false hi.Parent = game.Workspace
+hi:Play() wait(.46)
+Notification:Notify( {Title = "Credits", Description = "Made by InfinityMercury "}, {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 8, Type = "image"}, {Image = "http://www.roblox.com/asset/?id=7733964640", ImageColor = Color3.fromRGB(2, 255, 23), Callback = function(State) print(tostring(State)) end} ) wait(.25) workspace.Notification_Sound:Destroy()
+wait(1)
