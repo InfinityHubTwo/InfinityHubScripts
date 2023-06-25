dca9d8a36c2c822c5168b3063d05c4bb9217c9f3
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-06-25 19:52:25 -0300

    Update Main.lua

--- Games/World of Stands/Main.lua
+++ Games/World of Stands/Main.lua
@@ -1,218 +1,122 @@
---// Print
-print[[
-	:
-
-	--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>
-	|															  |
-	|	Infinity Hub |  InfHub Worlds Of Stans Update:  1.0.2 	  |
-	|	Infinity Hub |  Have Fun.								  |
-	|															  |
-	--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>
-
-]]
-
-
+-- notification
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+local hi = Instance.new("Sound")  hi.Name = "Notification_Sound"  hi.SoundId = "http://www.roblox.com/asset/?id=6026984224"  hi.Volume = 5  hi.archivable = false  hi.Parent = game.Workspace hi:Play() wait(.46)
+Notification:Notify(
+    {Title = "Script Executed", Description = [[
+- Infinity Hub Executed,
+- made by InfinityMercury, Darkzin and Cool
+    ]]},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 6, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=13780014144", ImageColor = Color3.fromRGB(255, 255, 255)}
+) wait(2)
 
 
---// not libray
-local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
 
---// Rayfiel Libray
-getgenv().SecureMode = true
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/InfinityHubTwo/InfinityHubScripts/main/Ui%20Libray/Rayfield/Main.lua'))()
-local Window = Rayfield:CreateWindow({
-   Name = "Infinity Hub  |  @Darkzin  ( Beta )",
-   LoadingTitle = "Infinity Hub",
-   LoadingSubtitle = "by darkzin",
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
-   },
-   Discord = {
-      Enabled = false,
-      Invite = "sirius", -- The Discord invite code, do not include discord.gg/
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-   KeySystem = true, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Infinity Hub",
-      Subtitle = "Key System",
-      Note = "Private Key",
-      FileName = "SiriusKey",
-      SaveKey = true,
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = "qy&nBDu=mWJkrJ9D4T7!=i&=7mBQ62"
-   }
+-- settings
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/InfinityHubTwo/InfinityHubScripts/main/Games/Stands%20Awakening/Webhook/Loader.lua'))();
+
+
+
+-- libray
+local repo = 'https://raw.githubusercontent.com/InfinityHubTwo/InfinityHubScripts/main/Ui%20Libray/Linoria/'
+local Libray = loadstring(game:HttpGet(repo .. 'Loader.lua'))()
+local ThemeManager = loadstring(game:HttpGet(repo .. 'ThemeMenager.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'SaveMenager.lua'))()
+local Window = Library:CreateWindow({
+    Title = 'Infinity Hub | '.. game:GetService('MarketplaceService'):GetProductInfo(game.PlaceId).Name or 'Game-'..game.PlaceId,
+    Center = true,
+    AutoShow = true,
+    TabPadding = 8,
+    MenuFadeTime = 0.2
 })
 
 
 
-
---// Variables
-local Settings 
-local plr = game:GetService("Players").LocalPlayer
-local Char = plr.Character
-local Hum = Char.Humanoid
-local Speed = Hum.WalkSpeed
-local Jump = Hum.JumpPower
-local Health = Hum.Health
-local hrp 					= game.Players.LocalPlayer.Character.HumanoidRootPart
-local TweenService 			= game:GetService('TweenService')
-local Player 				= game.Players.LocalPlayer
-local LocalPlayer 			= Player
-local Char 					= Player.Character or Player.CharacterAdded:wait()
+-- tabs
+local Tabs = {
+    Main = Window:AddTab('Main'),
+    ['UI Settings'] = Window:AddTab('UI Settings'),
+}
 
 
---// Function
-local function ChestFarm(a) 
-	for _, v in pairs(game:GetService("Workspace"):GetChildren()) do
-		if v:IsA("Model") and v.Name == a then
-			--< Plr Speed >--
-			pcall(
-				function ()
-					Char.Humanoid.WalkSpeed = 0
-				end
-			)
 
-			--< Move to Chest >--
-			local MoveToChect = TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(12), {CFrame = CFrame.new(v.Top.Position)})
-			MoveToChect:Play()
-			wait(12.8)
+-- code
+local MainBox = Tabs.Main:AddLeftGroupbox('Main')
+local Button = MainBox:AddButton({
+    Text = 'Win',
+    Func = function()
+        game.Players.LocalPlayer.Character:PivotTo(workspace.tower.finishes.Finish:GetPivot())
+    end,
+    DoubleClick = false,
+    Tooltip = 'Click to Win'
+})
+local Button = MainBox:AddButton({
+    Text = 'Win and rejoin',
+    Func = function()
+        game.Players.LocalPlayer.Character:PivotTo(workspace.tower.finishes.Finish:GetPivot()) wait(.25)
+        game.Players.LocalPlayer.Character:PivotTo(workspace.tower.finishes.Finish:GetPivot())
+        wait(2)
+        
+        local TeleportService = game:GetService("TeleportService"); 
+        local Players = game:GetService("Players"); 
+        if (#Players:GetPlayers() == 1) then 
+        game:GetService("Players").LocalPlayer:Kick(); 
+        TeleportService:Teleport(game.PlaceId); 
+        else 
+        TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId, Players:GetPlayers()[1]);
+        end
+     end,
+    DoubleClick = false,
+    Tooltip = 'Click to Win'
+})
+local Button = MainBox:AddButton({
+    Text = 'Infinite Jump',
+    Func = function()
+         local Player = game:GetService'Players'.LocalPlayer; local UIS = game:GetService'UserInputService'; _G.JumpHeight = 50; function Action(Object, Function) if Object ~= nil then Function(Object); end end UIS.InputBegan:connect(function(UserInput) if UserInput.UserInputType == Enum.UserInputType.Keyboard and UserInput.KeyCode == Enum.KeyCode.Space then Action(Player.Character.Humanoid, function(self) if self:GetState() == Enum.HumanoidStateType.Jumping or self:GetState() == Enum.HumanoidStateType.Freefall then Action(self.Parent.HumanoidRootPart, function(self) self.Velocity = Vector3.new(0, _G.JumpHeight, 0); end) end end) end end)
+    end,
+    DoubleClick = false,
+    Tooltip = 'Click to start a infinite jump'
+})
+local Button = MainBox:AddButton({
+    Text = 'Get all tools',
+    Func = function()
+        for _, v in pairs(game:GetService("ReplicatedStorage").Gear:GetChildren()) do
+          if v:IsA("Tool") then
+            cloneTools = v:Clone()
+            cloneTools.Parent = game:GetService("Players").LocalPlayer.Backpack
+          end
+        end
+    end,
+    DoubleClick = false,
+    Tooltip = 'Click to get all tools'
+})
+MainBox:AddToggle('GD', {
+    Text = 'God Mode',
+    Default = false,
+    Tooltip = 'Click to active god mode',
 
-			
-			--< Teleport Chest >--
-			--Char:PivotTo(v:GetPivot());
-			
-			
-			--< Collect Chest >--
-			fireproximityprompt(v.Inside.ProximityPrompt)
-		end
-	end
-end
-local function CheckChest(a)
-	for _, v in pairs(game:GetService("Workspace"):GetChildren()) do
-		if v:IsA("Model") and v.Name == a then
-			print(a)
-			ChestFarm(a)
-			
-		else
-			
-			print("Chest:", a, "Not Spawned")
-		end
-	end
-end
-local function AutoAllChests()
-	if CheckChest("1") then
-		print(".")
-	end
-	if CheckChest("2") then
-		print(".")
-	end
-	if CheckChest("3") then
-		print(".")
-	end
-	if CheckChest("4") then
-		print(".")
-	end
-	if CheckChest("5") then
-		print(".")
-	end
-	if CheckChest("6") then
-		print(".")
-	end
-	if CheckChest("7") then
-		print(".")
-	end
-	if CheckChest("8") then
-		print(".")
-	end
-	if CheckChest("9") then
-		print(".")
-	end
-	if CheckChest("10") then
-		print(".")
-	end
-	if CheckChest("11") then
-		print(".")
-	end
-	if CheckChest("12") then
-		print(".")
-	end
-	if CheckChest("13") then
-		print(".")
-	end
-	if CheckChest("14") then
-		print(".")
-	end
-	if CheckChest("15") then
-		print(".")
-	end
-	if CheckChest("16") then
-		print(".")
-	end
-end
-local function FeRespawn()
-	if game:GetService("Workspace").FilteringEnabled == true then
-		game:GetService("StarterGui"):SetCore("SendNotification", {
-			Title 	= 	" Player Respawn ";
-			Text 	= 	" Player Respawned for desactive Chest Farm. ";
-			Icon 	= 	"http://www.roblox.com/asset/?id=12828674545";
-		})
-	end
-	game.Players.LocalPlayer.Character.Head:Destroy()
-end
+    Callback = function(state)
+        settings = state
+        if settings then
+            while wait() and settings do
+	            repeat task.wait() until game.Players.LocalPlayer.Character:FindFirstChild("hitbox") or game.Players.LocalPlayer.Character:FindFirstChild("hitboxInvincible") 
+	            for i, p in ipairs(game.Players.LocalPlayer.Character:GetChildren()) do 
+	            	if (p.Name:find("hitbox")) then 
+	            		p:Destroy()
+	            	end
+	            end
+            end
 
+        else
 
---// Anti Teleport
-local mt = getrawmetatable(game)
-local oldhook = mt.__namecall
-
-setreadonly(mt, false)
-mt.__namecall = newcclosure(function(self, ...)
-if not checkcaller() and getnamecallmethod() == "Teleport" then
-return nil
-end
-return oldhook(self, ...)
-end)
-setreadonly(mt, true)
-local f
-f = hookmetamethod(game:GetService("TeleportService"), "__namecall", function(...)
-    if getnamecallmethod() == "Teleport" then
-        return
+            game:GetService("Players").LocalPlayer.Character.Head:Destroy()
+        end
     end
-
-    return f(...)
-end)
-
-
---// Libray Windown 
-local Tab = Window:CreateTab("Farming", 7743866529)
-local Paragraph = Tab:CreateParagraph({Title = "Chests Name:", Content = [[
-
-	--< 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 >--
-]]})
-
-local Input = Tab:CreateInput({
-   Name = "Select the chest you want to farm.",
-   PlaceholderText = "Chest Name",
-   RemoveTextAfterFocusLost = false,
-   Callback = function(Text)
-		CheckChest(Text)
-   end,
 })
-local Toggle = Tab:CreateToggle({
-   Name = "Farm All Chests",
-   CurrentValue = false,
-   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(State)
-		Settings = State
-		if Settings then
-			AutoAllChests()
 
-		else
-			
-			FeRespawn()
-		end
-	end,
-})
+
+
+-- libray settings
+local MenuGroup = Tabs['UI Settings']:AddLeftGroupbox('Menu')
+MenuGroup:AddButton('Unload', function() Library:Unload() end)
