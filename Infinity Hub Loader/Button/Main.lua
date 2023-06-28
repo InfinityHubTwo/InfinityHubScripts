f75982d893be4dcd7afc5e82b7000282b91ef24b
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-06-28 19:42:32 -0300

    Update Main.lua

--- Infinity Hub Loader/Button/Main.lua
+++ Infinity Hub Loader/Button/Main.lua
@@ -1,13 +1,11 @@
--- (VOID) : Gui to Lua
--- Version: 1.4
-
--- Instances:
+--[[
+Inifnity Hub Button Close / Open Hub
+By InfinityMercury
+]]
 
 local Main = Instance.new("ScreenGui")
 local Open = Instance.new("ImageButton")
 local UICorner = Instance.new("UICorner")
-local Close = Instance.new("ImageButton")
-local UICorner_2 = Instance.new("UICorner")
 
 --Properties:
 
@@ -21,22 +19,11 @@ Open.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Open.BackgroundTransparency = 1.000
 Open.Position = UDim2.new(0.0592734218, 0, 0.098484844, 0)
 Open.Size = UDim2.new(0, 49, 0, 48)
-Open.Visible = false
-Open.Image = "http://www.roblox.com/asset/?id=13404230870"
+Open.Visible = true
+Open.Image = "http://www.roblox.com/asset/?id=7733964640"
 
 UICorner.CornerRadius = UDim.new(1, 8)
 UICorner.Parent = Open
-
-Close.Name = "Close"
-Close.Parent = Main
-Close.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Close.BackgroundTransparency = 1.000
-Close.Position = UDim2.new(0.0592734218, 0, 0.098484844, 0)
-Close.Size = UDim2.new(0, 49, 0, 48)
-Close.Image = "http://www.roblox.com/asset/?id=13404230870"
-
-UICorner_2.CornerRadius = UDim.new(1, 8)
-UICorner_2.Parent = Close
 
 -- Scripts:
 
@@ -44,23 +31,11 @@ local function BZPQCRF_fake_script() -- Open.Main
 	local script = Instance.new('LocalScript', Open)
 
 	local a = script.Parent
-	local b = script.Parent.Parent.Close
 	a.MouseButton1Click:Connect(function ()
-		game:GetService("CoreGui").Rayfield.Enabled = true
-		a.Visible = false
-		b.Visible = true
+        local KeyPress = function(v)
+           return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
+        end
+        KeyPress("J")
 	end)
 end
 coroutine.wrap(BZPQCRF_fake_script)()
-local function SKPDGJ_fake_script() -- Close.Main 
-	local script = Instance.new('LocalScript', Close)
-
-	local a = script.Parent
-	local b = script.Parent.Parent.Open
-	a.MouseButton1Click:Connect(function ()
-		game:GetService("CoreGui").Rayfield.Enabled = false
-		a.Visible = false
-		b.Visible = true
-	end)
-end
-coroutine.wrap(SKPDGJ_fake_script)()
