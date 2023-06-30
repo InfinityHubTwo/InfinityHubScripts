2d4ba3164a1f4fb631a097a58f440d0c66cb6307
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-06-30 16:53:15 -0300

    Update MainCode.lua

--- Games/Heaven Stands/MainCode.lua
+++ Games/Heaven Stands/MainCode.lua
@@ -30,11 +30,8 @@ end
 
 -- insta kill bosses
 if getgenv().Heaven_Stands["Insta Kill Bosses"]["DIO"] then
-    --sound
-    local hi = Instance.new("Sound") hi.Name = "Notification_Sound" hi.SoundId = "http://www.roblox.com/asset/?id=6026984224" hi.Volume = 5 hi.archivable = false hi.Parent = game.Workspace
-    
-    --notification
-    hi:Play() wait(.46)
+    local hi = Instance.new("Sound") hi.Name = "Notification_Sound" hi.SoundId = "http://www.roblox.com/asset/?id=5153734236" hi.Volume = 5 hi.archivable = false hi.Parent = game.Workspace
+    hi:Play() wait(.30)
     Notification:Notify(
         {Title = "Insta Kill", Description = "Started Insta Kill Boss"},
         {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
@@ -43,14 +40,11 @@ if getgenv().Heaven_Stands["Insta Kill Bosses"]["DIO"] then
     
     -- kill
     local Mob = "DIO"
-    task.spawn(function ()     repeat task.wait()         Enemies = workspace.Enemies:GetChildren()         for i = 1, #Enemies do             local v = Enemies[i]             if                 v.Name == Mob and v:IsA("Model") and v:FindFirstChild("HumanoidRootPart") and                     v:FindFirstChildOfClass("Humanoid").Health > 0              then                 game.Players.LocalPlayer.Character.PrimaryPart.CFrame =                     CFrame.new(v.PrimaryPart.Position + Vector3.new(0, 7, 0), v.PrimaryPart.Position)             end         end     until 1+1==2 end) local ohString1 = "MouseButton1" game:GetService("ReplicatedStorage").Remote_Events.Input_Remote:InvokeServer(ohString1) wait(.25) workspace.Enemies.DIO.Head:Destroy()
+    task.spawn(function () repeat task.wait() Enemies = workspace.Enemies:GetChildren() for i = 1, #Enemies do local v = Enemies[i] if v.Name == Mob and v:IsA("Model") and v:FindFirstChild("HumanoidRootPart") and v:FindFirstChildOfClass("Humanoid").Health > 0 then game.Players.LocalPlayer.Character.PrimaryPart.CFrame = CFrame.new(v.PrimaryPart.Position + Vector3.new(0, 7, 0), v.PrimaryPart.Position) end end until 1+1==2 end) local ohString1 = "MouseButton1" game:GetService("ReplicatedStorage").Remote_Events.Input_Remote:InvokeServer(ohString1) wait(.25) workspace.Enemies.DIO.Head:Destroy()
 end
 if getgenv().Heaven_Stands["Insta Kill Bosses"]["Garou"] then
-    --sound
-    local hi = Instance.new("Sound") hi.Name = "Notification_Sound" hi.SoundId = "http://www.roblox.com/asset/?id=6026984224" hi.Volume = 5 hi.archivable = false hi.Parent = game.Workspace
-
-    --notification
-    hi:Play() wait(.46)
+    local hi = Instance.new("Sound") hi.Name = "Notification_Sound" hi.SoundId = "http://www.roblox.com/asset/?id=5153734236" hi.Volume = 5 hi.archivable = false hi.Parent = game.Workspace
+    hi:Play() wait(.30)
     Notification:Notify(
         {Title = "Insta Kill", Description = "Started Insta Kill Boss"},
         {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
@@ -59,14 +53,11 @@ if getgenv().Heaven_Stands["Insta Kill Bosses"]["Garou"] then
     
     -- kill
     local Mob = "Garou"
-    task.spawn(function ()     repeat task.wait()         Enemies = workspace.Enemies:GetChildren()         for i = 1, #Enemies do             local v = Enemies[i]             if                 v.Name == Mob and v:IsA("Model") and v:FindFirstChild("HumanoidRootPart") and                     v:FindFirstChildOfClass("Humanoid").Health > 0              then                 game.Players.LocalPlayer.Character.PrimaryPart.CFrame =                     CFrame.new(v.PrimaryPart.Position + Vector3.new(0, 7, 0), v.PrimaryPart.Position)             end         end     until 1+1==2 end) local ohString1 = "MouseButton1" game:GetService("ReplicatedStorage").Remote_Events.Input_Remote:InvokeServer(ohString1) wait(.25) workspace.Enemies.Garou.Head:Destroy()
+    task.spawn(function () repeat task.wait() Enemies = workspace.Enemies:GetChildren() for i = 1, #Enemies do local v = Enemies[i] if v.Name == Mob and v:IsA("Model") and v:FindFirstChild("HumanoidRootPart") and v:FindFirstChildOfClass("Humanoid").Health > 0 then game.Players.LocalPlayer.Character.PrimaryPart.CFrame = CFrame.new(v.PrimaryPart.Position + Vector3.new(0, 7, 0), v.PrimaryPart.Position) end end until 1+1==2 end) local ohString1 = "MouseButton1" game:GetService("ReplicatedStorage").Remote_Events.Input_Remote:InvokeServer(ohString1) wait(.25) workspace.Enemies.Garou.Head:Destroy()
 end
 if getgenv().Heaven_Stands["Insta Kill Bosses"]["Vergil"] then
-    --sound
-    local hi = Instance.new("Sound") hi.Name = "Notification_Sound" hi.SoundId = "http://www.roblox.com/asset/?id=6026984224" hi.Volume = 5 hi.archivable = false hi.Parent = game.Workspace
-
-    --notification
-    hi:Play() wait(.46)
+    local hi = Instance.new("Sound") hi.Name = "Notification_Sound" hi.SoundId = "http://www.roblox.com/asset/?id=5153734236" hi.Volume = 5 hi.archivable = false hi.Parent = game.Workspace
+    hi:Play() wait(.30)
     Notification:Notify(
         {Title = "Insta Kill", Description = "Started Insta Kill Boss"},
         {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
@@ -75,7 +66,22 @@ if getgenv().Heaven_Stands["Insta Kill Bosses"]["Vergil"] then
     
     -- kill
     local Mob = "Vergil"
-    task.spawn(function ()     repeat task.wait()         Enemies = workspace.Enemies:GetChildren()         for i = 1, #Enemies do             local v = Enemies[i]             if                 v.Name == Mob and v:IsA("Model") and v:FindFirstChild("HumanoidRootPart") and                     v:FindFirstChildOfClass("Humanoid").Health > 0              then                 game.Players.LocalPlayer.Character.PrimaryPart.CFrame =                     CFrame.new(v.PrimaryPart.Position + Vector3.new(0, 7, 0), v.PrimaryPart.Position)             end         end     until 1+1==2 end) local ohString1 = "MouseButton1" game:GetService("ReplicatedStorage").Remote_Events.Input_Remote:InvokeServer(ohString1) wait(.25) workspace.Enemies.Vergil.Head:Destroy()
+    task.spawn(function () repeat task.wait() Enemies = workspace.Enemies:GetChildren() for i = 1, #Enemies do local v = Enemies[i] if v.Name == Mob and v:IsA("Model") and v:FindFirstChild("HumanoidRootPart") and v:FindFirstChildOfClass("Humanoid").Health > 0 then game.Players.LocalPlayer.Character.PrimaryPart.CFrame = CFrame.new(v.PrimaryPart.Position + Vector3.new(0, 7, 0), v.PrimaryPart.Position) end end until 1+1==2 end) local ohString1 = "MouseButton1" game:GetService("ReplicatedStorage").Remote_Events.Input_Remote:InvokeServer(ohString1) wait(.25) workspace.Enemies.Vergil.Head:Destroy()
+end
+if getgenv().Heaven_Stands["Insta Kill Bosses"]["Silver Chariot Requiem"] then
+    local hi = Instance.new("Sound") hi.Name = "Notification_Sound" hi.SoundId = "http://www.roblox.com/asset/?id=5153734236" hi.Volume = 5 hi.archivable = false hi.Parent = game.Workspace
+    hi:Play() wait(.30)
+    Notification:Notify(
+        {Title = "Insta Kill", Description = "Started Insta Kill Boss"},
+        {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
+        {Image = "http://www.roblox.com/asset/?id=7733964640", ImageColor = Color3.fromRGB(2, 255, 78), Callback = function(State) print(tostring(State)) end}
+    ) wait(.25) workspace.Notification_Sound:Destroy()
+    
+    -- kill
+    workspace.Enemies["Silver Chariot Requiem"].Highlight:Destroy()
+    wait(.25)
+    local Mob = "Silver Chariot Requiem"
+    task.spawn(function () repeat task.wait() Enemies = workspace.Enemies:GetChildren() for i = 1, #Enemies do local v = Enemies[i] if v.Name == Mob and v:IsA("Model") and v:FindFirstChild("HumanoidRootPart") and v:FindFirstChildOfClass("Humanoid").Health > 0 then game.Players.LocalPlayer.Character.PrimaryPart.CFrame = CFrame.new(v.PrimaryPart.Position + Vector3.new(0, 7, 0), v.PrimaryPart.Position) end end until 1+1==2 end) local ohString1 = "MouseButton1" game:GetService("ReplicatedStorage").Remote_Events.Input_Remote:InvokeServer(ohString1) wait(.25) workspace.Enemies["Silver Chariot Requiem"].Head:Destroy()
 end
 
 
