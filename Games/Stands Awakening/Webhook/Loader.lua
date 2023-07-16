541729dc0867ff110682dc91a529a47b0a1085bd
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-07-16 12:36:01 -0300

    Delete Games/Stands Awakening/Webhook directory

--- Games/Stands Awakening/Webhook/Loader.lua
+++
@@ -1,2 +0,0 @@
-local Executor = identifyexecutor()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Rylvns/EnvisionExploits/master/utility-scripts/DiscordWebhook-Poster-Preset.lua"))()("https://discord.com/api/webhooks/1120487967245475980/2aWLRDZp0Gp2A1ZL7pImpdmMfwOzPIPgITt2qxYEm_aRyj1Ebf6HsJgbn3_E59zXYMfP", {"Executor Using: ".. Executor});
