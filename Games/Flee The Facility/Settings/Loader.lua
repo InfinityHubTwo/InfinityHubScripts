6621adc9ebee2720d7d55b81d6a750dfec869b71
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-07-18 21:08:58 -0300

    Update Loader.lua

--- Games/Flee The Facility/Settings/Loader.lua
+++ Games/Flee The Facility/Settings/Loader.lua
@@ -1,8 +1,10 @@
 -- Settings
 a = 'patched'
 b = 'unknown'
+c = 'ModuleScript'
 
 warn('Get Free Vip: '.. a)
 warn('Infinite Money: '.. a)
 warn('Money Negative: '.. a)
 warn('Get Money: '.. a)
+warn('all scripts are not working because of the (script path): '.. c)
