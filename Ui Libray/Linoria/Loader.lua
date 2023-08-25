47a6c5f5c592cd4e24fa85f30855edbc52fe593f
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-08-25 15:57:58 -0300

    Update Loader.lua

--- Ui Libray/Linoria/Loader.lua
+++ Ui Libray/Linoria/Loader.lua
@@ -30,9 +30,9 @@ local Library = {
     HudRegistry = {};
 
     FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(28, 28, 28);
-    BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(255, 255, 255);
+    MainColor = Color3.fromRGB(35, 35, 48);
+    BackgroundColor = Color3.fromRGB(29, 27, 38);
+    AccentColor = Color3.fromRGB(66, 110, 135);
     OutlineColor = Color3.fromRGB(50, 50, 50);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
